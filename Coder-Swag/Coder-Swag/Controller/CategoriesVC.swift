8966a13af2190441dc99068bf663246c79fd884c
Frederik Nagels <nagels97@live.dk>
2018-07-09 21:17:53 +0200

    Lesson 01

---
+++ Coder-Swag/Coder-Swag/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Full On Pimp on 09/07/2018.
+//  Copyright © 2018 Nagels. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
